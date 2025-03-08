217d32f5e46693062eec5a1251b7789a2d5fe0cc
Prashant-Kumar-104 <72803281+Prashant-Kumar-104@users.noreply.github.com>
2025-03-08 13:03:49 +0000

    updated rg name for remote tfstate file

--- environments/dev/provider.tf
+++ environments/dev/provider.tf
@@ -7,7 +7,7 @@ terraform {
     }
   }
   backend "azurerm" {
-    resource_group_name   = "rg-terraform-state"
+    resource_group_name   = "rg-terraform-remote-state"
     storage_account_name  = "stterraformstate759"
     container_name        = "tfstate"
     key                   = "dev.terraform.tfstate"
