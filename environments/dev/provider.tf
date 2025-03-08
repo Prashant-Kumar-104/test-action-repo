89d6682e6a89d2858c8e4fe6507cfc028daf29dc
Prashant-Kumar-104 <72803281+Prashant-Kumar-104@users.noreply.github.com>
2025-03-08 13:00:53 +0000

    remote-state

--- environments/dev/provider.tf
+++ environments/dev/provider.tf
@@ -6,6 +6,13 @@ terraform {
       version = "4.19.0"
     }
   }
+  backend "azurerm" {
+    resource_group_name   = "rg-terraform-state"
+    storage_account_name  = "stterraformstate759"
+    container_name        = "tfstate"
+    key                   = "dev.terraform.tfstate"
+    
+  }
 }
 
 provider "azurerm" {
